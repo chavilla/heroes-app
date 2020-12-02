7dc04e928149f04d523e0b89e323dbc8c7d1835f
chavilla <jcharris.villa@gmail.com>
2020-12-02 13:51:50 -0500

    Ajustes

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroById } from '../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
 
     const { heroId }=useParams();
 
-    const hero=getHeroById(heroId);
+    const hero=useMemo(() => getHeroById(heroId), [heroId]);
 
     if (!hero) {
         return <Redirect to='/' />
     }
 
-    const { alter_ego, characters, first_appearance, superhero }=hero;
+    const handleReturn=()=>{
+
+        if (history.length <=2) {
+            history.push('/');
+        }else{
+            history.goBack();
+        }
+
+    }
+
+    const { alter_ego, characters, publisher ,first_appearance, superhero }=hero;
 
     return (
-        <div>
-            <h1>HeroScreen</h1>
-            <p>{alter_ego}</p>
+        <div className='row mt-5'>
+            <div className='col-md-4'>
+                <img src={ `../assets/${heroId}.jpg` } className='img-thumbnail animate_animated animate__fadeInLeft' alt={ superhero } />
+            </div>
+            <div className='col-md-8'>
+                <h3>{superhero}</h3>
+                <ul className='list-group list-group-flush'>
+                    <li className='list-group-item'><b>Alter ego: { alter_ego } </b></li>
+                    <li className='list-group-item'><b>Publisher: { publisher } </b></li>
+                    <li className='list-group-item'><b> { first_appearance }</b></li>
+                </ul>
+                <h5>characters</h5>
+                <p>{characters}</p>
+                <button className='btn btn-primary'
+                onClick={ handleReturn }
+                >Return</button>
+            </div>
         </div>
     )
 }
