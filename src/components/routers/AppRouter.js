354e6406aee1c7cf5b2fffc07171bd8f4f516f46
chavilla <jcharris.villa@gmail.com>
2020-12-05 16:09:33 -0500

    changes

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import PrivateRoutes from "./PrivateRoutes";
 import { LoginScreen } from "../login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
